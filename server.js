4270e4ff3461e43ddc213f120d0e4c48ddc80f4e
PrabhatAcharya <code2prabhat@gmail.com>
2023-04-01 12:48:36 +0530

    pRODuPDATE

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import Connection from './database/db.js';
 import DefaultData from './default.js';
 import Route from './routes/routes.js';
 import dotenv from 'dotenv';
-const path = require('path');
+
 
 const app = express();
 dotenv.config();
@@ -13,13 +13,6 @@ dotenv.config();
 
 app.use(cors());
 app.use('/',Route);
-
-//static files
-app.use(express.static(path.join(__dirname,'../client/build')))
-
-app.get('*',function(req, res){
-    res.sendFile(path.join(__dirname,'../client/build/index.html'));
-})
 
 const PORT = process.env.PORT || 8080;
 const username=process.env.DB_USERNAME;
